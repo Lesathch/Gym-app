be92f64f7de9b80c150daa5479d4697eeefaa8c3
Raquel <raquelalejandra2012@gmail.com>
2026-03-04 15:50:19 -0500

    Front and back update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,14 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5104', 
+        secure: false
+      }
+    }
+  }
 });
+ 
